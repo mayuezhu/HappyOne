867abd51a7b165474d82acb5e5abbf629ec8bc75
dc10 <mayuezhu0530@gmail.com>
2015-10-11 09:36:21 +0000

    Added index action and updated link

--- happyone_saas/app/controllers/users_controller.rb
+++ happyone_saas/app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  
+  def index
+    @users = User.includes(:profile)
+  end
+  
   def show
     @user = User.find( params[:id] )
   end
